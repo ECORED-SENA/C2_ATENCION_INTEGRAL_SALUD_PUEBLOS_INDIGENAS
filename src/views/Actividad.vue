9d98ffe6731bb93937d5db0d590d90881ffd4ff7
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-03-18 14:37:49 -0500

    Ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,30 +1,28 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(icono="fas fa-puzzle-piece" titulo="Actividad didáctica")
+  BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
-    .titulo-segundo.color-secundario
-      h2 Emparejamiento de conceptos
-    
-    .row
-      .col-lg-3
-        figure
-            img(src="@/assets/curso/activ.svg", alt="Texto que describa la imagen")
-      
-      .col-lg-9.d-flex
-        div(style="position: relative")
-          .row
-            .col-lg-7
-              p.mb-0 Conceptualizar las bases para la atención integral de los pueblos indígenas en aislamiento
 
-          .tarjeta.tarjeta--azul.p-3(style="background-color: #F7724A; width: 100%; position: absolute; top: 65%")
-            .row.justify-content-around.align-items-center            
-              .col-sm.mb-3.mb-sm-0
-                h4.mb-0.fw-bold.mb-0 Cuestionario
-              .col-auto                
-                  a.acti.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
-                    span Realizar
-                    i.fas.fa-puzzle-piece    
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+          figure
+            img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo.color-secundario
+          h2  Actividad didáctica 1
+        p.mb-4 Objetivo: conceptualizar las bases para la atención integral de los pueblos indígenas en aislamiento.
+        .tarjeta.tarjeta--azul.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Conceptos de los pueblos indígenas
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
+                span Relacionar términos
+                i.fas.fa-puzzle-piece
+    
+    
+
 
 
 </template>
